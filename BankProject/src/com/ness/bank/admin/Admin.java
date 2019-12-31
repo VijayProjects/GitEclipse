12cea907dc278e77b566b89cd9d92b25f456c5e9
vijay <vijaybabu.k007@gmail.com>
2019-12-31 13:13:38 +0530

    div() is added to Admin class by TL

--- BankProject/src/com/ness/bank/admin/Admin.java
+++ BankProject/src/com/ness/bank/admin/Admin.java
@@ -1,6 +1,10 @@
 package com.ness.bank.admin;
 
 public class Admin {
+	public int div(int x, int y) {
+		return x / y;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Admintest");
 	}
